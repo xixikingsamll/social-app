5f072c98d643ce267b8c611acf2d67c723d48356
xixikingsamll <yongxiyue@126.com>
2024-12-08 11:11:57 +0800

    update

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,10 +1,28 @@
 const mysql = require('mysql2')
 
-const db = mysql.createPool({ 
-    host: 'localhost', 
-    user: 'root', 
-    password: '123456', 
-    database: 'nodejs' 
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'nodejs'
 })
+
+// 开发人员1
+// const db = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '123456',
+//     database: 'nodejs'
+// })
+
+// 开发人员2
+// const db = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '123456',
+//     database: 'nodejs'
+// })
+
+
 
 module.exports = db;
